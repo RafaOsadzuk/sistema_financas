c9f4b1e9944ffa00624ffc21fd055e9dbe272ab4
Rafael Osadzuk <rafael.osadzuk@gmail.com>
2024-06-21 20:26:30 -0300

    fazendo o botão de cadastrar saldo funcionar liberando input e imprimindo o valor na tela, não permitindo que seja adicionado valores não numericos

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 sistema criado como projeto da faculdade cujo objetivo é gerir finanças pessoais utilizando as tecnologias PHP, HTML, CSS e JavaScript. O sistema permitirá a inserção e gerenciamento de receitas e despesas. 
 Integrantes:
 João Barros Guatura da Costa - 22013545-2
-Rafael Souza Osadzuk -
+Rafael Souza Osadzuk - 22137934-2
 Raffael Guidetti Mielo -
 Vinicius Kawasugui Santiago -
 Leonardo Pedrozo -
--- src/public/app.js
+++ src/public/app.js
@@ -1,5 +1,56 @@
 const saldoText = document.getElementById('saldoText');
-const mostrarInputButton = document.getElementById('saldoInput');
+const saldoInput = document.getElementById('saldoInputButton');
 
-mostrarInputButton.addEventListener('click', function() {
+
+saldoInput.addEventListener('click', function() {
+   
+    const input = document.createElement('input');
+    input.type = 'text';
+
+  
+    input.addEventListener('keydown', function(event) {
+        
+        if (!(/[0-9]/.test(event.key) || event.key === 'Enter' || event.key === 'Backspace' || event.key === 'ArrowLeft' || event.key === 'ArrowRight')) {
+       
+            alert('Apenas números são permitidos.');
+            event.preventDefault();
+        }
+        
+        
+        if (event.key === 'Enter') {
+            adicionarSaldo(input.value);
+        }
+        
+        
+    });
+
+    
+    saldoText.removeChild(saldoInputButton);
+    saldoText.appendChild(input);
+
+   
+    input.focus();
 });
+
+function adicionarSaldo(valor) {
+    
+    console.log('Saldo adicionado:', valor);
+
+   
+    const novoTexto = document.createElement('span');
+    novoTexto.textContent = 'Saldo adicionado: ' + valor;
+    
+    saldoText.textContent = ''; 
+    saldoText.appendChild(novoTexto);
+    
+    
+    const novoBotao = document.createElement('button');
+    novoBotao.textContent = 'cadastrar Saldo';
+    novoBotao.id = 'saldoInputButton';
+    
+    
+    novoBotao.addEventListener('click', function() {
+        saldoText.textContent = 'Saldo: ';
+        saldoText.appendChild(novoBotao);
+    });
+}

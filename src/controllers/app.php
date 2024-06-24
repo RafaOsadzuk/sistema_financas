01a93ff656a142af81445b4a478bc0057ba53c56
Raffael Guideti Miello <raffaelguideti@gmail.com>
2024-06-23 23:54:27 -0300

    Modificação para conexão no banco de dados e inserção de saldo
    tentativa de adicionar a integração com o banco de dados

--- src/controllers/app.php
+++ src/controllers/app.php
@@ -6,6 +6,7 @@ require_once __DIR__ . 'src\controllers\Despesa.php';
 require_once __DIR__ . 'src\controllers\Saldo.php';
 require_once __DIR__ . 'src\models\SaldoModel.php';
 
+/*
 if (isset($_POST['saldo'])) {
     
     $saldoValue = filter_var($_POST['saldo'], FILTER_SANITIZE_NUMBER_FLOAT, FILTER_FLAG_ALLOW_FRACTION);
@@ -25,4 +26,35 @@ if (isset($_POST['saldo'])) {
 } else {
     http_response_code(400);
     echo json_encode(['message' => 'Saldo não enviado via formulário.']);
+    
+}
+*/
+
+$pdo = Database::getConn();
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+ 
+    //$aluno = new Aluno($_POST['nome'], $_POST['Ra'], $_POST['email']);
+
+    $saldo = new Saldo($_POST['saldo']);
+    
+   
+    $saldoModel = new SaldoModel($saldo);
+    if ($saldoModel->save()) {
+        
+        exit();
+    } else {
+        
+        echo "Não foi possível salvar o aluno no banco de dados.";
+    }
+} else if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    
+    $stmt = $pdo->query('SELECT saldo FROM SaldoUsuario');
+    $saldo = $stmt->fetchAll(PDO::FETCH_ASSOC);
+    
+    header('Content-Type: application/json');
+    echo json_encode($saldo);
+    exit();
+} else {
+    echo "Impossível conectar no banco";
 }
